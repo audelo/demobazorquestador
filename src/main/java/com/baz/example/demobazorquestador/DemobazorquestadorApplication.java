b98cb2b7b6f36947c7bf256955a360f569ca15ce
git <morloragus@gmail.com>
2018-07-05 11:49:21 -0500

    Push Proyect

---
+++ src/main/java/com/baz/example/demobazorquestador/DemobazorquestadorApplication.java
@@ -0,0 +1,12 @@
+package com.baz.example.demobazorquestador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobazorquestadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemobazorquestadorApplication.class, args);
+	}
+}
